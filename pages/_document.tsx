e61e59efddd31f074a69adc17685573f4402854d
daksh <akshd@outlook.com>
2024-08-11 11:40:17 +0530

    chore: add title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>nebula protocol</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
